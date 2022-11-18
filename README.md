033fd7356a9375bbd3bec1fedf49a3d55a740153
Johan Karlsson <johan.k0528@gmail.com>
2022-11-18 12:38:38 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Standard Deviation Calculator
+
+Calculates the mean and deviation of a sample provided in `sample.txt`
+
+Accepts both comma and dot for decimals
---
+++ main.py
@@ -0,0 +1,41 @@
+import math
+
+
+def main():
+    sample = []
+
+    with open("sample.txt", "r") as f:
+        sample = f.read().splitlines()
+        for i in range(len(sample)):
+            sample[i] = sample[i].replace(",", ".")
+
+    print(f"Mean: {calc_mean(sample)} \nDeviation: {calc_deviation(sample)}")
+
+
+def calc_deviation(sample):
+    # Calculate the mean
+    sample_sum = 0
+    for i in range(len(sample)):
+        sample[i] = float(sample[i])
+        sample_sum += sample[i]
+
+    mean = sample_sum / len(sample)
+
+    deviation = 0
+    for i in range(len(sample)):
+        deviation += (sample[i] - mean) ** 2
+
+    return round(math.sqrt(deviation / len(sample)), 5)
+
+
+def calc_mean(sample):
+    sample_sum = 0
+    for i in range(len(sample)):
+        sample[i] = float(sample[i])
+        sample_sum += sample[i]
+
+    return round(sample_sum / len(sample), 5)
+
+
+if __name__ == "__main__":
+    main()
